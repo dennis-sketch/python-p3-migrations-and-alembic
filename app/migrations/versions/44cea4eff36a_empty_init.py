cbddd22511a4d6b5b8e6ce43cfb110778d105ff3
dennis-sketch <dennis.kimani2@student.moringaschool.comgit config --global user.name dennis-sketch>
2023-12-07 19:15:01 +0300

    completed assignment

---
+++ app/migrations/versions/44cea4eff36a_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 44cea4eff36a
+Revises: 8493596cb922
+Create Date: 2023-12-07 19:12:01.567646
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '44cea4eff36a'
+down_revision = '8493596cb922'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ app/migrations/versions/8493596cb922_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 8493596cb922
+Revises: 
+Create Date: 2023-12-07 19:07:15.344738
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8493596cb922'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
